e7a23bbc0cc28283f85e80c60dda3b269f4389d0
ryuzaku <satria.b2698@gmail.com>
2024-04-03 10:58:34 +0700

    Add Scoped

--- Program.cs
+++ Program.cs
@@ -11,6 +11,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ITokoRepository, TokoRepository>();
+builder.Services.AddScoped<IPenjualanProdukRepository, PenjualanProdukRepository>();
+builder.Services.AddScoped<IPenjualanRepository, PenjualanRepository>();
+builder.Services.AddScoped<IProdukRepository, ProdukRepository>();
+builder.Services.AddScoped<IRegisterRepository, RegisterRepository>();
 builder.Services.AddDbContext<TokoSayaContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("TokoSayaContext"))
 );
